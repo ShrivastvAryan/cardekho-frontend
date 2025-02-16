17d71563184074f4f501ce17db1e775f7b43e698
Aryan Shrivastava <asguy29@gmail.com>
2025-02-16 17:34:45 +0530

    Ui update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: '',
+          hostname: 'cdn4.singleinterface.com',
         },
       ],
     },
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import Image from 'next/image'
-import { Link, Tabs,Tab,TabList,Grid,GridItem } from "@chakra-ui/react"
+import { Link, Tabs,Tab,TabList,Grid,GridItem,Select,Button } from "@chakra-ui/react"
 
 const mainPage=()=>{
   return(
     <>
-    <div className= "w-full h-80">
+    <div className="flex">
+      <div className="w-72 h-80 bg-gray-200 ml-2 mr-3 rounded-md my-5 p-2">
+        <p className="font-semibold text-lg text-center">Search By Car Brand</p>
+        <Select bg="white" className="mt-2 font-semibold">
+        <option value='M'>Maruti Suzuki</option>
+        <option value='H'>Hyundai</option>
+        <option value='TM'>Tata Motors</option>
+        <option value='M&M'>Mahindra</option>
+        <option value='VW'>Volkswagen</option>
+        </Select>
+        <Button colorScheme='red' size="sm" className=" mt-3">Search</Button>
+      </div>
+      <div>
       <Image 
-      src="https://s3-ap-southeast-1.amazonaws.com/assetsin.izmocars.com/b_images/harrier-16393-12554.jpg"
+      src="https://cdn4.singleinterface.com/files/enterprise/coverphoto/76409/tata-17-04-24-10-55-58.jpg"
       alt="Tata poster"
-      width={500}
-      height={300}/>
+      width={1000}
+      height={550}
+      className="content-end"/>
+      </div>
     </div>
-    <div className="my-10 p-2 bg-gray-100 w-11/12 h-80 mx-auto rounded-md" >
+    <div className="my-5 p-2 bg-gray-100 w-11/12 h-80 mx-auto rounded-md" >
       <div>
       <Tabs colorScheme="red">
       <TabList className="font-semibold">
