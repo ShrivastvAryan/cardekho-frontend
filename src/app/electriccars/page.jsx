051d404ca41a485f2996e718f9932fa02aec4d4e
Aryan Shrivastava <asguy29@gmail.com>
2025-04-11 00:18:28 +0530

    added all data

--- src/app/electriccars/page.jsx
+++ src/app/electriccars/page.jsx
@@ -1,39 +1,54 @@
+"use client"
 import React from 'react'
 import { Link, Tabs,Tab,TabList,Grid,GridItem,Select,Button } from "@chakra-ui/react"
+import{useState,useEffect} from "react";
+import Image from 'next/image'
 
 const electricCars=()=>{
+
+    const [carList, setCarList] = useState([]); //for API
+
+     useEffect(() => {
+        const fetchCars = async () => {
+          try {
+            const response = await fetch('/data/car.json'); // Replace with your API URL
+            const data = await response.json();    
+
+            const electricCars = data.filter(car => car.fuel==="Electric");
+            setCarList(electricCars);
+          } catch (error) {
+            console.error("Error fetching cars:", error);
+          }
+        };
+    
+        fetchCars();
+      }, [])
+
+
     return(
         <>
-         <div className="my-5 p-2 bg-gray-100 w-11/12 h-auto mx-auto rounded-md" >
-              <div>
-              <Tabs colorScheme="green">
-              <TabList className="font-semibold">
-              <Tab>All</Tab>
-              <Tab>Hatchback</Tab>
-              <Tab>Sedan</Tab>
-              <Tab>SUV</Tab>
-              <Tab>MUV</Tab>
-              <Tab>Luxury</Tab>
-              </TabList>
-              </Tabs>
-              </div>
-              <div>
-              <Grid templateColumns='repeat(4, 1fr)' gap={4} className="p-4">
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              <GridItem w='92%' h='60' bg='gray.200' className="rounded-md" />
-              </Grid>
-              </div>
-            </div>
+              <div className="my-5 p-2 bg-gray-100 w-11/12 h-auto mx-auto rounded-md">
+                 <Grid templateColumns="repeat(4, 1fr)" gap={5} className="p-2 my-2 ">
+                   {carList.map((list, index) => (
+                   <GridItem key={index}  w="90%" h="60" bg="gray.200" className="rounded-md cursor-pointer">
+                   <div className="p-2 w-full bg-red-500 h-3/5 relative">
+                  <Image
+                    src={list.image}
+                    alt={list.name}
+                    fill
+                    className="object-cover rounded-md"
+                   />
+                   </div>
+        
+                   <div className="p-2 font-semibold text-xl">
+                   <p className="font-extrabold">{list.name}</p>
+                   <p className="text-sm mt-1.5">Mileage:{list.mileage}</p>
+                   <p className="text-sm mt-1.5">Price:{list.price}</p>
+                   </div>
+                  </GridItem>
+                 ))}
+            </Grid>
+          </div>
         </>
     )
 }
--- src/app/ui/mid-hero.jsx
+++ src/app/ui/mid-hero.jsx
@@ -1,9 +1,14 @@
+"use client"
 import React from "react";
 import Image from 'next/image'
 import { Link, Tabs,Tab,TabList,Grid,GridItem,Select,Button } from "@chakra-ui/react"
+import{useState,useEffect} from "react";
 
 
 const MidHero=()=>{
+
+  const [carList, setCarList] = useState([]); //for API
+  const [selectedType, setSelectedType] = useState("All"); //for specific selection
 
   const carTab=[
     {label:'All', value:'All'},
@@ -14,18 +19,35 @@ const MidHero=()=>{
     {label:'Luxury',value:'Luxury'}
   ]
 
-  const carList=[
-    {name:'Tata Curvv ',mileage:'15-16kmpl',price:'10-19 Lakh',image:'/curvv.jpeg'}
+  useEffect(() => {
+    const fetchCars = async () => {
+      try {
+        const response = await fetch('/data/car.json'); // Replace with your API URL
+        const data = await response.json();
 
-  ]
+        const filteredCars =
+        selectedType === "All"
+          ? data.slice(0,8)
+          : data.filter(car => car.type === selectedType);
+
+
+          setCarList(filteredCars);
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+      }
+    };
+
+    fetchCars();
+  }, [selectedType]);
+
     return(
         <>
-         <div className="my-5 p-2 bg-gray-100 w-11/12 h-80 mx-auto rounded-md" >
+         <div className="my-5 p-2 bg-gray-100 w-11/12 h-auto mx-auto rounded-md" >
               <div>
               <Tabs colorScheme="red">
               <TabList className="font-semibold">
               {carTab.map((tab)=>(
-                <Tab key={tab.value} value={tab.value}>{tab.label}</Tab>
+                <Tab key={tab.value} value={tab.value} onClick={()=>setSelectedType(tab.value)}>{tab.label}</Tab>
               ))}
               </TabList>
               </Tabs>
@@ -33,7 +55,7 @@ const MidHero=()=>{
 
 
         <div>
-         <Grid templateColumns="repeat(4, 1fr)" gap={3} className="p-2 my-2">
+         <Grid templateColumns="repeat(4, 1fr)" gap={5} className="p-2 my-2">
            {carList.map((list, index) => (
            <GridItem key={index} w="90%" h="60" bg="gray.200" className="rounded-md cursor-pointer">
            <div className="p-2 w-full bg-red-500 h-3/5 relative">
