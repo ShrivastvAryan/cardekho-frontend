ea13ae11350a91a46362de8babeae3aec59972ac
Aryan Shrivastava <asguy29@gmail.com>
2025-04-11 11:34:40 +0530

    added dyanmic routing

---
+++ src/app/car/[brand]/id/page.jsx
@@ -0,0 +1,37 @@
+"use client"
+import React from 'react'
+import { Link, Tabs,Tab,TabList,Grid,GridItem,Select,Button } from "@chakra-ui/react"
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import Image from 'next/image'
+
+
+const CarPage=()=>{
+
+    const { brand } = req.query
+    const [carData, setCarData] = useState(null);
+
+    useEffect(() => {
+        if (brand) {
+          fetch(`/data/car?${brand}`)
+            .then(res => res.json())
+            .then(data => setCarData(data))
+            .catch(err => console.error(err));
+        }
+      }, [brand]);
+    
+      if (!carData) return <div>Loading...</div>;
+
+    return(
+        <>
+         <div>
+      <h1>{carData.name}</h1>
+      <p>Brand: {carData.brand}</p>
+      <p>ID: {carData.id}</p>
+      {/* More details */}
+    </div>
+        </>
+    )
+}
+
+export default CarPage
