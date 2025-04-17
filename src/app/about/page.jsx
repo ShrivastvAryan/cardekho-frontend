becf77550d58ae0c95c1b3a3f2abec3006409674
Aryan Shrivastava <asguy29@gmail.com>
2025-04-17 13:20:15 +0530

    updated ui

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,70 @@
 import React from 'react'
+import { Github,Linkedin,Instagram,Twitter } from 'lucide-react';
+import { Button } from '@chakra-ui/react';
+
 
 const aboutUs=()=>{
+
+    const Links=[
+        {label:'Github',link:'',image:<Github/>},
+        {label:'LinkedIn',link:'',image:<Linkedin />},
+        {label:'Instagram',link:'',image:<Instagram/>},
+        {label:'Twitter',link:'',image:<Twitter/>}
+    ] 
+
     return(
         <>
-        <div className='text-4xl text-red-500 flex justify-center align-center'>
-            Hi there this website is made so that you can search your favorite car across your favorite brand
+        <div className='text-4xl text-red-500 flex justify-center align-center py-4'>
+            Contact Form
         </div>
+
+        <div className='md:flex '>
+        <div className="border-[1px] border-black p-2 m-2 rounded-md md:w-1/2">
+            <form>
+            <div className="flex justify-between mt-4">
+            <label htmlFor='username'>Username:</label>
+             <input type='text' name="username" id="username" 
+        autoComplete="off" required className='bg-red-100 pl-2 rounded-lg w-full ml-2 '/>
+            </div>
+        
+            <div className="flex justify-between   mt-4">
+            <label htmlFor='email'>Email:</label>
+            <input type='text' name="email" id="email" 
+            autoComplete="off" required className='bg-red-100 pl-2  mt-2 rounded-lg w-full ml-8'/>
+            </div>
+
+            <div className="flex justify-between w-full  mt-4">
+            <label htmlFor='message'>Message:</label>
+            <textarea
+            name="message"
+            id="message"
+            cols="25"
+            rows='4'
+            className='bg-red-100 pl-2  mt-2 rounded-lg w-full ml-2'
+            />
+            </div>
+
+            <div className="flex justify-center items-center pt-4">
+            <Button type="submit" colorScheme='red' className="text-black p-2 cursor-pointer rounded-lg font-semibold">Submit</Button>
+            </div>
+
+
+            </form>
+        </div>
+
+        <div className=" border-[1px] border-black p-2 m-2 rounded-md md:w-1/2 md:flex md:justify-center md:items-center">
+            <div className='flex justify-evenly gap-2'>
+            {Links.map((icon,index)=>(
+                <div key={index} className='rounded-full w-16 h-16 lg:w-28 lg:h-28 text-2xl bg-red-100 '>{icon.image}</div>
+            ))   
+            }
+            </div>
+
+        </div>
+        </div>
+
+        
+       
         </>
     )
 }
